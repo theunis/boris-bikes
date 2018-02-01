0296a649a8509972088c38d7878c683046be6c38
Van Vliet, Theun <theun.vanvliet@sky.uk>
2018-02-01 14:15:30 +0000

    add link to source data

--- 20-preprocess/21-load.R
+++ 20-preprocess/21-load.R
@@ -1,3 +1,4 @@
+# data from http://cycling.data.tfl.gov.uk/
 journey_data <- read_csv('00-data/18JourneyDataExtract10Aug2016-16Aug2016.csv')
 library(XML)
 # download.file('https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml',
